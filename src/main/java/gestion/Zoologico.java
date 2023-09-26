7b3420e212ac6cfd37e3c16a6408695d9a2af94c
LuaN1409 <141962309+LuaN1409@users.noreply.github.com>
2023-09-26 09:34:31 -0500

    Paquetes, clases y subclases

---
+++ src/main/java/gestion/Zoologico.java
@@ -0,0 +1,55 @@
+package gestion;
+import java.util.ArrayList;
+
+public class Zoologico {
+
+	private String nombre;
+	private String ubicacion;
+	private ArrayList<Zona> zonas = new ArrayList<>();
+	
+    public Zoologico() {
+        this(null, null);
+    }
+
+    public Zoologico(String nombre, String ubicacion) {
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+    }
+
+    public void agregarZonas(Zona zona) {
+        this.zonas.add(zona);
+        zona.setZoo(this);
+    }
+
+    public int cantidadTotalAnimales() {
+        int contador = 0;
+        for (int i=0; i< zonas.size();i++) {
+        	contador += this.zonas.get(i).cantidadAnimales();
+        }
+        return contador;		
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+    
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public ArrayList<Zona> getZona() {
+        return zonas;
+    }
+
+    public void setZona(ArrayList<Zona> zonas) {
+        this.zonas = zonas;
+    }
+}
